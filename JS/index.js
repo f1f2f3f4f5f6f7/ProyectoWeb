8dd917b84927f92cabe6a5999e73a1aa48f62c70
Kevin <castrokevin312@gmail.com>
2023-10-29 16:45:14 -0500

    *Primera entrega proyetoWeb

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+const mostrarInput = document.getElementById('mostrar-input');
+const campoTexto = document.getElementById('campo-texto');
+
+mostrarInput.addEventListener('click', function() {
+    campoTexto.style.display = 'block';
+});
