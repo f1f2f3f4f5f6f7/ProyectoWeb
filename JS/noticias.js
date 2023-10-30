dc2fffd355c2cd1cd423356a0abb96ba10777106
Kevin <castrokevin312@gmail.com>
2023-10-29 21:59:59 -0500

    Ahora si, lo mismo del navbar solo kite algunas cosas no importantes

--- JS/noticias.js
+++ JS/noticias.js
@@ -1,33 +1,19 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     var formData = JSON.parse(localStorage.getItem("formData"));
-
     var usernameContainer = document.getElementById("username-container");
-
     var logoutButton = document.getElementById("logout-button");
-
     var signInLink = document.getElementById("sign-in-button");
 
-    // Si hay datos de formulario en el localStorage, oculta el enlace de "Sign In"
     if (formData) {
-        // Si hay datos de formulario en el localStorage, oculta el enlace de "Sign In"
         signInLink.style.display = "none";
-
-        // Accede al elemento HTML por su ID y actualiza su contenido con el username
         usernameContainer.textContent = "¡Bienvenido, " + formData.username + "!";
-        
-        // Muestra el botón de "Cerrar Sesión"
         logoutButton.style.display = "block";
     } else {
-        // Si no hay datos de formulario en el localStorage, no se muestra ningún mensaje
-        // y se oculta el botón de "Cerrar Sesión"
         logoutButton.style.display = "none";
     }
-
     document.getElementById("logout-button").addEventListener("click", function() {
-        // Limpia los datos del formulario del localStorage al hacer clic en "Cerrar Sesión"
         localStorage.removeItem("formData");
-
         window.location.reload();
     });
 });
--- JS/registro.js
+++ JS/registro.js
@@ -1,28 +1,21 @@
 document.getElementById("enviarBtn").addEventListener("click", function(event) {
     event.preventDefault();
-
     var loginExitoso = true;
 
     if (loginExitoso) {
-        // Obtiene los valores del formulario
         var username = document.getElementById("username").value;
         var email = document.getElementById("email").value;
         var password = document.getElementById("password").value;
 
-        // Crea un objeto con los datos del formulario
         var formData = {
             username: username,
             email: email,
             password: password
         };
 
-        // Guarda el objeto completo en el localStorage
         localStorage.setItem("formData", JSON.stringify(formData));
-
-        // Redirige al usuario a la página principal
         window.location.href = '../index.html';
     } else {
-        // Maneja el caso cuando el inicio de sesión no es exitoso
         console.log("Inicio de sesión fallido");
     }
 });
